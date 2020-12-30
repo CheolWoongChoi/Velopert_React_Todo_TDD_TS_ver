f8c2f03931cf446879a4be8758fd43f3b5c9df4e
CheolWoong <bluesky4381@naver.com>
2020-12-30 13:34:28 +0900

    ADD: 리액트 TS 프로젝트 셋팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const App = () => {
+	return (
+		<>
+			App test
+		</>
+	)
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = function(env, argv) {
+	const devMode = env.development ? true : false;
+
+	return {
+		mode: devMode ? 'development' : 'production',
+		resolve: {
+			extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			alias: {
+				src: path.resolve(__dirname, 'src'),
+				public: path.resolve(__dirname, 'public'),
+				images: path.resolve(__dirname, 'public/images'),
+				constants: path.resolve(__dirname, 'src/constants'),
+				components: path.resolve(__dirname, 'src/components'),
+				actionTypes: path.resolve(__dirname, 'src/store/actionTypes'),
+				actions: path.resolve(__dirname, 'src/store/actions'),
+				reducers: path.resolve(__dirname, 'src/store/reducers'),
+			}
+		},
+		entry: {
+			app: './index'
+		},
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'build'),
+		},
+		devServer: {
+			port: 3031,
+			hot: true,
+			open: true,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: [
+						{ 
+							loader: 'ts-loader'
+						},
+					]
+				},
+				{
+					test: /\.s[ac]ss$/,
+					use: [
+						devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 
+						{
+							loader: 'css-loader',
+							options: { url: false, sourceMap: true } 
+						},
+						'sass-loader'
+					]
+				},
+				{
+					test: /\.(jpe?g|png|gif|svg)$/,
+					use: [{
+						loader: 'file-loader',
+						options: {
+							esModule: false
+						}
+					}]
+				}
+			]
+		},
+		plugins: [
+			new CleanWebpackPlugin(),
+			new HtmlWebpackPlugin({
+				filename: 'index.html',
+				template: './index.html',
+			}),
+			new MiniCssExtractPlugin({ 
+				filename: '[name].css',
+				chunkFilename: '[id].css'
+			}),
+			new CopyWebpackPlugin({
+				patterns: [
+					{ from: 'public', to: 'public' }
+				]
+			})
+		]
+	}
+}
